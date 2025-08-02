2a946dc6827e6aa4964ced9f94c73cdced4b01ed
Erick <trendztienda@gmail.com>
2025-08-02 14:52:39 -0500

    Actualización de las listas

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // variables
 let nombres = [];
 
-function agregarAmigos() {
-    document.getElementById(amigo);
-    if(agregarAmigos == '') {
-        alert 'Por favor, inserte su nombre';
-    } else {
-        nombres.push(agregarAmigos);
-        )
+
+
+//funcion para agregar nombres
+function agregarAmigo () {
+   let nombreAgregado = document.getElementById("amigo").value;
+  
+   if (nombreAgregado === '') {
+    alert('Por favor, inserte un nombre')
+   }
+    nombres.push(nombreAgregado);
+    actualizarLista();
+    console.log(nombres);
+    document.getElementById("amigo").value= '';
+}   
+
+//funcion para actualizar la lista 
+function actualizarLista() {
+    let listaActualizada = document.getElementById("listaAmigos");
+    listaActualizada.innerHTML = '';
+    for (let nombre of nombres) {
+        let lista = document.createElement('li');
+        lista.textContent = nombre;
+        listaActualizada.appendChild(lista);
     }
 }
-
-// Validar entrada, (si el campo está vacio retonar un "alert", si no agregar el avlor al array.) 
-// ¿Como? lo averiguaremos 
