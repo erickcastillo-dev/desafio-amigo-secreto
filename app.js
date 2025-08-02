e7b5e2c53193256a738e02e98c36a7975952e6fe
erickcastillo-dev <trendztienda@gmail.com>
2025-08-02 12:51:37 -0500

    Funcion validar nombre + alert

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// variables
+let nombres = [];
+
+function agregarAmigos() {
+    document.getElementById(amigo);
+    if(agregarAmigos == '') {
+        alert 'Por favor, inserte su nombre';
+    } else {
+        nombres.push(agregarAmigos);
+        )
+    }
+}
+
+// Validar entrada, (si el campo está vacio retonar un "alert", si no agregar el avlor al array.) 
+// ¿Como? lo averiguaremos 
